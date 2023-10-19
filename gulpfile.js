0c7917e96624ef38a5441ed0d8de97698adf15ea
SamSemRod <semyon.rodionov.03@mail.ru>
2023-10-19 20:37:51 +0300

    final commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 const gulp = require('gulp');
-// Создайте простую задачу
-gulp.task('hello', () => {
-    console.log('Hello, Gulp!');
-});
 
-// Задача по умолчанию
-gulp.task('default', gulp.series('hello'));
+gulp.task('hello', () => {
+    console.log('Hello, World!');
+});
